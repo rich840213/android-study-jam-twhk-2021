f8fd154adf86ed14110144efaa78a3c0b8ad24a0
“e <rich840213@gmail.com>
2021-04-24 22:56:06 +0800

    Get user input in an app: Part 2

--- app/src/main/java/jk/test/androidstudyjamtwhk2021/TipTimeActivity.kt
+++ app/src/main/java/jk/test/androidstudyjamtwhk2021/TipTimeActivity.kt
@@ -1,6 +1,10 @@
 package jk.test.androidstudyjamtwhk2021
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import jk.test.androidstudyjamtwhk2021.databinding.ActivityTipTimeBinding
 import java.text.NumberFormat
@@ -20,10 +24,14 @@ class TipTimeActivity : AppCompatActivity() {
 
     private fun initListener() {
         binding.calculateButton.setOnClickListener { calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
         if (cost == null) {
             binding.tipResult.text = ""
@@ -44,5 +52,16 @@ class TipTimeActivity : AppCompatActivity() {
 
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER || keyCode == KeyEvent.ACTION_DOWN) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
